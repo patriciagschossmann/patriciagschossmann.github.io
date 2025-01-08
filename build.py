79dc3da917828d4817a7d22fd25f4c4b5ca276b7
Patricia Gschoßmann <patricia.gschossmann@gmail.com>
2025-01-08 11:33:17 +0100

    Add research interests

--- build.py
+++ build.py
@@ -4,11 +4,15 @@ from pybtex.database.input import bibtex
 def get_personal_data():
     name = ["Patricia", "Gschoßmann"]
     email = "patricia.gschossmann@gmail.com"
-    github = "patriciamdr"
+    github = "patriciagschossmann"
     bio_text = f"""
                 <p>
                 I'm a PhD student at the <a href="https://uni-tuebingen.de/en/" target="_blank">University of Tübingen</a> in the <a href="https://uni-tuebingen.de/en/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet/fachbereiche/informatik/lehrstuehle/autonomous-vision/home/" target="_blank">Autonomous Vision Group</a>, led by <a href="https://www.cvlibs.net/" target="_blank">Prof. Dr.-Ing. Andreas Geiger</a>.
                 My research is supported by the <a href="https://imprs.is.mpg.de/" target="_blank">International Max Planck Research School of Intelligent Systems<a/>.
+                </p>
+                <p>
+                My research interests lie at the intersection of computer vision (3D vision), machine learning (deep learning), and computer graphics (geometry, rendering), in particular 3D reconstruction from 2D images and novel view synthesis.
+                I am particularly interested in improving 3D scene representation and related tasks such as surface meshing and scene relighting.
                 </p>
                 <p>
                 Before starting my PhD, I worked as a computer vision and deep learning intern for augmented reality at <a href="https://www.bmw.de/de/home.html" target="_blank">BMW</a> on the AR glasses research project, which was showcased at <a href="https://www.press.bmwgroup.com/global/article/detail/T0438824EN/generative-ai-augmented-reality-and-teleoperated-parking-%E2%80%93-the-digital-experience-in-the-bmw-of-the-future-at-the-consumer-electronics-show-ces-2024?language=en" target="_blank">CES 2024<a/>.
